9f0fc52411d1cc6567e78b32c52e66c70c11c95c
Vasyl Subota <kennixer@gmail.com>
2026-08-17 17:59:16 +0300

    Functional Programming: tailrec fibonacci

--- week-1/functional-programming/project/src/main/kotlin/Main.kt
+++ week-1/functional-programming/project/src/main/kotlin/Main.kt
@@ -5,10 +5,11 @@ fun main() {
 
     println(fibonacciLoop(30))
     println(fibonacciRecursive(30))
+    println(fibonacciTailRecursive(30))
 }
 
 fun fibonacciLoop(n: Int): Int {
-    val list = ArrayList<Int>(n)
+    val list = arrayListOf<Int>()
     for (i in 0..n) {
         if (i < 2) list.add(i)
         else list.add(list[i - 1] + list[i - 2])
@@ -18,4 +19,10 @@ fun fibonacciLoop(n: Int): Int {
 
 fun fibonacciRecursive(n: Int): Int {
     return if (n < 2) n else fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
+}
+
+fun fibonacciTailRecursive(n: Int): Int = fibonacciTailRec(n)
+
+private tailrec fun fibonacciTailRec(n: Int, first: Int = 0, second: Int = 1): Int {
+    return if (n == 0) first else fibonacciTailRec(n - 1, second, first + second)
 }
